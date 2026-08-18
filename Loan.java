7e3375d22fa1c01eb872c9854ab174845434d440
Sima-Kumari <simak4484@gmail.com>
2026-08-18 22:52:52 +0530

    Add files via upload

---
+++ Loan.java
@@ -0,0 +1,20 @@
+public class Loan {
+   
+    public static void main(String[] args) {
+        int age = 22;
+        int salary = 35000;
+        if(age > 21 && salary > 25000) {
+            System.out.println("Eligible for Loan");
+
+        } else {
+             System.out.println("Not Eligible for Loan");
+        }
+    }
+}
+
+    
+
+  
+
+
+
