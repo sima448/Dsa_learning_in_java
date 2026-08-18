c51da18bbebef516c8fbd61abe33baa291112ae0
Sima-Kumari <simak4484@gmail.com>
2026-08-18 22:52:02 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,12 @@
+public class Conditional {
+    
+    public static void main(String[] args) {
+        int a = 11;
+        if(a < 10) {
+            System.out.println("A is Less than 10");
+
+        } else {
+             System.out.println("A is Greater Than 10");
+        }
+    }
+}
